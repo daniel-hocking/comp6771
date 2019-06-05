ea43b25d3283f43da9e11ce92a4905cac51aeb0b
daniel_hocking <daniel.hocking@gmail.com>
2019-06-05 11:11:06 +1000

    Work on tute week1 addnumbers and cat

--- tutorials/week1/addnumbers.cpp
+++ tutorials/week1/addnumbers.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+int main() {
+  std::cout << "Please enter two numbers: ";
+  int int_a, int_b;
+  std::cin >> int_a >> int_b;
+  std::cout << "Sum: " << int_a + int_b << std::endl;
+  return 0;
+}
--- tutorials/week1/cat.cpp
+++ tutorials/week1/cat.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  for (string line; getline(cin, line);) {
+    cout << line << endl;
+  }
+  return 0;
+}
